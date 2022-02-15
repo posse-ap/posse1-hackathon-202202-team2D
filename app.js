2ff52ec3f96b47c4106e120c1090fd9f8ff93e23
hiroki-osuga68 <osugahi1006@keio.jp>
2022-02-16 05:41:27 +0900

    mainのレスポンシブ

--- app.js
+++ app.js
@@ -226,12 +226,14 @@ let resultArea = document.getElementById("result_area");
 かもりゅう
 ============================ */
 let btn_start = document.getElementById('btn_start');
+let progress_list = document.querySelector('.progress_list');
 btn_start.addEventListener('click',function(){
     let main_center = document.getElementById('main_center');
     let main_center_day1 = document.getElementById('main_center_day1');
     main_center.classList.add('fade_out');
     main_center_day1.classList.remove('fade_out');
     main_center_day1.classList.add('fade_in');
+    progress_list.style.display = "flex";
 
 
 })
