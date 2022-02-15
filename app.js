0103371c08541c54f99eb289e163b12c99b821f4
加茂竜之介 <Kamoryu2001@gmail.com>
2022-02-15 10:08:55 +0900

    #2 startボタンクリック時に、start画面はfade_out、診断画面にfade_in追加

--- app.js
+++ app.js
@@ -7,6 +7,18 @@
 /* ============================
 かもりゅう
 ============================ */
+let btn_start = document.getElementById('btn_start');
+btn_start.addEventListener('click',function(){
+    let main_center = document.getElementById('main_center');
+    let main_center_day1 = document.getElementById('main_center_day1');
+    main_center.classList.add('fade_out');
+    main_center_day1.classList.remove('fade_out');
+    main_center_day1.classList.add('fade_in');
+
+
+})
+
+
 /* ============================
 よしたか
 ============================ */
